719005075a9961df92d5f68d22b41a2af268b6b7
CypHry <cyprianhryniuk@gmail.com>
2019-04-09 17:52:03 +0200

    saving qstring to file methods added

--- inc/weather_data_saver.h
+++ inc/weather_data_saver.h
@@ -14,8 +14,28 @@ class weather_data_saver : public QObject
     Q_OBJECT
 
 public:
-    int save_to_file(const weather_data& data, const QString& filename);
-    int save_to_file(const weather_data& data, QFile& file);
+    int save_data_to_file(const weather_data& data, const QString& filename);
+    int save_data_to_file(const weather_data& data, QFile& file);
+
+    /** Saving QString to file.
+     *
+     * Creates QFile with filename and writes string to it.
+     *
+     * @param string
+     * @param filename
+     * @retval 0 - saved successfully
+     * @retval -1 - error occurred
+     */
+    int save_json_string_to_file(const QString& string, const QString& filename);
+
+    /** Saving QString to file.
+     *
+     * @param string - string to be saved
+     * @param file
+     * @retval 0 - saved successfully
+     * @retval -1 - error occurred
+     */
+    int save_json_string_to_file(const QString& string, QFile& file);
 };
 
 #endif // WEATHER_DATA_SAVER_H
--- src/weather_data_saver.cpp
+++ src/weather_data_saver.cpp
@@ -1,3 +1,29 @@
 #include "weather_data_saver.h"
 
+int weather_data_saver::save_json_string_to_file(const QString& string, QFile& file)
+{
+    if(file.open(QIODevice::WriteOnly))
+    {
+        file.write(string.toUtf8());
+        file.close();
 
+        return 0;
+    }
+
+    return -1;
+}
+
+int weather_data_saver::save_json_string_to_file(const QString& string, const QString& filename)
+{
+    QFile file(filename);
+
+    if(file.open(QIODevice::WriteOnly))
+    {
+        file.write(string.toUtf8());
+        file.close();
+
+        return 0;
+    }
+
+    return -1;
+}
