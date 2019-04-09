2c51c3aa22c0931191494f14bc840c5851d1c4e0
CypHry <cyprianhryniuk@gmail.com>
2019-04-09 17:19:37 +0200

    weather_data doxygen update

--- inc/weather_data.h
+++ inc/weather_data.h
@@ -35,17 +35,77 @@ private:
     int snow;
 
 public:
-    /** Saving weather data to file.
+    /** city_name getter
      *
-     * @retval 0 if saved successfully
-     * @retval -1 if error occurred
+     * @return city_name
      */
-    int save_to_file() const;
+    const QString get_city_name() const {return city_name;}
 
-    /** Reading weather data from file.
+    /** date getter
      *
+     * @return date
      */
-    void read_from_file();
+    const QString get_date() const {return date;}
+
+    /** hour getter
+     *
+     * @return hour
+     */
+    const QString get_hour() const {return hour;}
+
+    /** description getter
+     *
+     * @return description
+     */
+    const QString get_description() const {return description;}
+
+    /** temperature getter
+     *
+     * @return temp
+     */
+    const double get_temp() const {return temp;}
+
+    /** pressure getter
+     *
+     * @return pressure
+     */
+    const int get_pressure() const {return pressure;}
+
+    /** humidity getter
+     *
+     * @return humidity
+     */
+    const int get_humidity() const {return humidity;}
+
+    /** wind_speed getter
+     *
+     * @return wind_speed
+     */
+    const double get_wind_speed() const {return wind_speed;}
+
+    /** wind_deg getter
+     *
+     * @return wind_deg
+     */
+    const int get_wind_deg() const {return wind_deg;}
+
+    /** clouds getter (in %)
+     *
+     * @return clouds
+     */
+    const int get_clouds() const {return clouds;}
+
+    /** rain getter
+     *
+     * @return rain
+     */
+    const int get_rain() const {return rain;}
+
+    /** snow getter
+     *
+     * @return snow
+     */
+    const int snow() const {return snow;}
 
 public slots:
     /** Parsing data from QString
