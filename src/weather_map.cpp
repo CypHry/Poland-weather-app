42fdb3ad5463735a510663d02e3a1777e2547e19
CypHry <cyprianhryniuk@gmail.com>
2019-04-09 22:07:00 +0200

    text test

--- src/weather_map.cpp
+++ src/weather_map.cpp
@@ -3,7 +3,8 @@
 weather_map::weather_map()
 {
     scene = new QGraphicsScene(this);
-    QPixmap pix();
-    pix.fill(Qt::red);
+    QPixmap pix("img/map_of_poland.png");
     map_img = scene->addPixmap(pix);
+    temp = scene->addText("test");
+    temp->setPos(150,150);
 }
