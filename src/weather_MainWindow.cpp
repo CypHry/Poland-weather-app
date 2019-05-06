68e3d8ea7df9e519a2053115a2ee7c60b809bb8e
CypHry <cyprianhryniuk@gmail.com>
2019-05-06 15:07:23 +0200

    sets current date and time while starting app

--- src/weather_MainWindow.cpp
+++ src/weather_MainWindow.cpp
@@ -9,4 +9,6 @@ weather_MainWindow::weather_MainWindow(QMainWindow* parent) : QMainWindow(parent
     QObject::connect(Ui.date_edit2, SIGNAL(dateChanged(QDate)), Ui.date_edit, SLOT(setDate(QDate)));
     QObject::connect(Ui.time_edit, SIGNAL(timeChanged(QTime)), Ui.time_edit2, SLOT(setTime(QTime)));
     QObject::connect(Ui.time_edit2, SIGNAL(timeChanged(QTime)), Ui.time_edit, SLOT(setTime(QTime)));
+    Ui.date_edit->setDate(QDate::currentDate());
+    Ui.time_edit->setTime(QTime::currentTime());
 }
