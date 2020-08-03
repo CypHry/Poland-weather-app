e3d4c144146cc402ab7fceea83ebe330b4def592
Cyprian Hryniuk <cyprianhryniuk@gmail.com>
2020-08-03 08:00:59 +0200

    unnecessary ifs removed

--- src/weather_forecaster.cpp
+++ src/weather_forecaster.cpp
@@ -14,19 +14,7 @@ weather_forecaster::weather_forecaster()
 void weather_forecaster::save_data()
 {
     QString filename;
-    QString city_name;
-    if(data->get_city() == "Wroclaw")
-        city_name = "Wroclaw ";
-    else if(data->get_city() == "Warsaw")
-        city_name = "Warsaw ";
-    else if(data->get_city() == "Katowice")
-        city_name = "Katowice ";
-    else if(data->get_city() == "Poznan")
-        city_name = "Poznan ";
-    else if(data->get_city() == "Gdansk")
-        city_name = "Gdansk ";
-    else if(data->get_city() == "Krakow")
-        city_name = "Krakow ";
+    QString city_name = data->get_city() + " ";
 
     for(unsigned int i = 0; i < data->get_dates().size(); i++)
     {
